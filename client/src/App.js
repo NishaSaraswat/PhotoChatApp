8cae91f591407e82904d4f693640ec03ae3afaa9
nisha saraswat <nisha0217@gmail.com>
2021-05-19 09:29:15 +0200

    axios installed and updated register page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from "./Pages/LoginPage";
-import ProfilePage from './Pages/ProfilePage';
+import ProfilePage from "./Pages/ProfilePage";
 import Home from './Pages/HomePage';
+import Registerpage from './Pages/RegisterPage';
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
               <Route exact path="/" component={Home}/>
               <Route exact path="/login" component={Login}/>
               <Route exact path='/profile' component={ProfilePage}/>
-              {/*<Route exact path='/register' component={RegisterUser}/>
-              */}
+              <Route exact path='/register' component={Registerpage}/>
+              
             </Switch>
         </>
         </Router>
