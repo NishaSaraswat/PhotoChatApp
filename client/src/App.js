c2f903d487afcafc2e3d91bceb3b89a1d589732e
Silvia02 <srj1213@gmail.com>
2021-05-20 22:24:23 +0200

    deleted errors

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,6 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from "./Pages/LoginPage";
-
-import ProfilePage from './Pages/ProfilePage';
-import RegisterPage from './Pages/RegisterPage';
-
-import ProfilePage from './Pages/ProfilePage';
-import RegisterPage from './Pages/RegisterPage';
-
-import ProfilePage from "./Pages/ProfilePage";
 import Home from './Pages/HomePage';
 import Registerpage from './Pages/RegisterPage';
 import CameraPage from './Pages/CameraPage';
@@ -25,11 +17,11 @@ function App() {
               <Route exact path="/" component={Home}/>
               <Route exact path="/login" component={Login}/>
 
-              <Route exact path='/register' component={RegisterPage}/>
+      
               
 
               <Route exact path='/camera' component={CameraPage}/>
-              <Route exact path='/register' component={Registerpage}/>
+        
               <Route exact path='/images' component={ImagesPage}/>
 
 
