7e8fdd9c28d559fb849522fc511eefbd82d840bc
nisha saraswat <nisha0217@gmail.com>
2021-05-20 21:51:41 +0200

    login style added

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from "axios";
+import {Box,Button,TextField} from "@material-ui/core";
 
 function Loginpage(props) {
     
@@ -16,23 +17,27 @@ function Loginpage(props) {
         }).then(response=>{
             response = response.data;
             console.log(response.data);
-            // localStorage.setItem("token",response.data.token);
-            // props.history.push("/profile");
+            localStorage.setItem("token",response.data.token);
+            props.history.push("/profile");
         }).catch(err=>{
           err = err.response.data;
         });
     };
 
     return (
-        <div>
-         <h1>Log in page</h1> 
-         <label>Email</label>
-         <input type = "text" placeholder="Enter your email" name="name" ref={emailRef}></input>
+        <div style={{textAlign:"center",lineHeight:"30px",backgroundColor:"white", fontSize:"12px",width:"240px",height:"350px",paddingLeft:"22px",marginLeft:"9px",marginTop:"100px",borderRadius:"15px"}}>
+         <h1 style={{paddingTop:'30px',color:"ThreeDDarkShadow",marginRight:"25px"}}>Log In</h1> 
+         <TextField   style={{marginRight:"20px",height:"40px",width:"200px",borderRadius:"8px"}} type = "text"  label="Enter your email"  name="name" ref={emailRef}></TextField>
          <br></br><br></br>
-         <label>Password </label>
-         <input type = "password" placeholder="password" name="password"  ref={passwordRef}></input>
+         <TextField style={{marginRight:"20px",height:"40px",width:"200px",borderRadius:"8px"}} type = "password" label="Enter password" name="password"  ref={passwordRef}></TextField>
          <br></br><br></br>
-         <button onClick={loginUser}>SignIn</button>
+         <Box style={{marginTop:"20px",marginRight:"20px",backgroundColor:"yellow",color:"green"}} clone>
+         <Button
+            variant = "contained"
+             size="large"
+             color = "primary"
+          onClick={loginUser}>SignIn</Button>
+          </Box>
         </div>
     )
 }
